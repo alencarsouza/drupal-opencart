a1e084501d3ba4dff8aa107503990998b2508858
Junior Holowka <junior.holowka@gmail.com>
2012-02-09 20:16:59 -0200

    First commit

---
+++ web/store/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://gonow.drupal/store/');
+define('HTTP_IMAGE', 'http://gonow.drupal/store/image/');
+define('HTTP_ADMIN', 'http://gonow.drupal/store/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://gonow.drupal/store/');
+define('HTTPS_IMAGE', 'http://gonow.drupal/store/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ajunior/Workspace/drupal/web/store/catalog/');
+define('DIR_SYSTEM', '/home/ajunior/Workspace/drupal/web/store/system/');
+define('DIR_DATABASE', '/home/ajunior/Workspace/drupal/web/store/system/database/');
+define('DIR_LANGUAGE', '/home/ajunior/Workspace/drupal/web/store/catalog/language/');
+define('DIR_TEMPLATE', '/home/ajunior/Workspace/drupal/web/store/catalog/view/theme/');
+define('DIR_CONFIG', '/home/ajunior/Workspace/drupal/web/store/system/config/');
+define('DIR_IMAGE', '/home/ajunior/Workspace/drupal/web/store/image/');
+define('DIR_CACHE', '/home/ajunior/Workspace/drupal/web/store/system/cache/');
+define('DIR_DOWNLOAD', '/home/ajunior/Workspace/drupal/web/store/download/');
+define('DIR_LOGS', '/home/ajunior/Workspace/drupal/web/store/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'gonow');
+define('DB_PASSWORD', 'gonow@123');
+define('DB_DATABASE', 'ftd');
+define('DB_PREFIX', 'oc_');
+?>
