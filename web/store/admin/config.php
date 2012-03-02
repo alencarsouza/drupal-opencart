7981adcdc68001ff19a0c98435747746e0025ce6
Junior Holowka <junior.holowka@gmail.com>
2012-03-02 14:43:42 -0300

    Atualizado

--- web/store/admin/config.php
+++ web/store/admin/config.php
@@ -9,17 +9,17 @@ define('HTTPS_SERVER', 'http://portal.ftd.com/store/admin/');
 define('HTTPS_IMAGE', 'http://portal.ftd.com/store/image/');
 
 // DIR
-define('DIR_APPLICATION', '/home/ajunior/Workspace/drupal/web/store/admin/');
-define('DIR_SYSTEM', '/home/ajunior/Workspace/drupal/web/store/system/');
-define('DIR_DATABASE', '/home/ajunior/Workspace/drupal/web/store/system/database/');
-define('DIR_LANGUAGE', '/home/ajunior/Workspace/drupal/web/store/admin/language/');
-define('DIR_TEMPLATE', '/home/ajunior/Workspace/drupal/web/store/admin/view/template/');
-define('DIR_CONFIG', '/home/ajunior/Workspace/drupal/web/store/system/config/');
-define('DIR_IMAGE', '/home/ajunior/Workspace/drupal/web/store/image/');
-define('DIR_CACHE', '/home/ajunior/Workspace/drupal/web/store/system/cache/');
-define('DIR_DOWNLOAD', '/home/ajunior/Workspace/drupal/web/store/download/');
-define('DIR_LOGS', '/home/ajunior/Workspace/drupal/web/store/system/logs/');
-define('DIR_CATALOG', '/home/ajunior/Workspace/drupal/web/store/catalog/');
+define('DIR_APPLICATION', '/home/ajunior/Workspace/ftd/web/store/admin/');
+define('DIR_SYSTEM', '/home/ajunior/Workspace/ftd/web/store/system/');
+define('DIR_DATABASE', '/home/ajunior/Workspace/ftd/web/store/system/database/');
+define('DIR_LANGUAGE', '/home/ajunior/Workspace/ftd/web/store/admin/language/');
+define('DIR_TEMPLATE', '/home/ajunior/Workspace/ftd/web/store/admin/view/template/');
+define('DIR_CONFIG', '/home/ajunior/Workspace/ftd/web/store/system/config/');
+define('DIR_IMAGE', '/home/ajunior/Workspace/ftd/web/store/image/');
+define('DIR_CACHE', '/home/ajunior/Workspace/ftd/web/store/system/cache/');
+define('DIR_DOWNLOAD', '/home/ajunior/Workspace/ftd/web/store/download/');
+define('DIR_LOGS', '/home/ajunior/Workspace/ftd/web/store/system/logs/');
+define('DIR_CATALOG', '/home/ajunior/Workspace/ftd/web/store/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
